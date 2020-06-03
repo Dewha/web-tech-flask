68de5aa90227008c17943e120adf7d01caa13a6c
Dewha <scherbinin96@gmail.com>
2020-06-03 17:45:06 +0500

    14

---
+++ 14/main2.py
@@ -0,0 +1,194 @@
+from flask import Flask, make_response, redirect, render_template, url_for, request, flash
+from flask_script import Manager, Command, Shell
+from forms import ContactForm
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.debug = True
+app.config['SECRET_KEY'] = 'a really really really really long secret key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:pass@localhost/flask_app_db'
+
+manager = Manager(app)
+db = SQLAlchemy(app)
+
+
+def updating_session():
+    # ...
+    return res
+
+
+class Category(db.Model):
+    __tablename__ = 'categories'
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    slug = db.Column(db.String(255), nullable=False)
+    created_on = db.Column(db.DateTime(), default=datetime.utcnow)
+    created_on = db.Column(db.DateTime(), default=datetime.utcnow)
+    posts = db.relationship('Post', backref='category')
+
+    def __repr__(self):
+        return "<{}:{}>".format(id, self.name)
+
+
+post_tags = db.Table('post_tags',
+                     db.Column('post_id', db.Integer, db.ForeignKey('posts.id')),
+                     db.Column('tag_id', db.Integer, db.ForeignKey('tags.id'))
+                     )
+
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer(), primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    slug = db.Column(db.String(255), nullable=False)
+    content = db.Column(db.Text(), nullable=False)
+    created_on = db.Column(db.DateTime(), default=datetime.utcnow)
+    updated_on = db.Column(db.DateTime(), default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    def __repr__(self):
+        return "<{}:{}>".format(self.id, self.title[:10])
+
+
+class Tag(db.Model):
+    __tablename__ = 'tags'
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    slug = db.Column(db.String(255), nullable=False)
+    created_on = db.Column(db.DateTime(), default=datetime.utcnow)
+    updated_on = db.Column(db.DateTime(), default=datetime.utcnow, onupdate=datetime.utcnow)
+    category_id = db.Column(db.Integer(), db.ForeignKey('categories.id'))
+    created_on = db.Column(db.DateTime(), default=datetime.utcnow)
+    posts = db.relationship('Post', secondary=post_tags, backref='tags')
+
+    def __repr__(self):
+        return "<{}:{}>".format(id, self.name)
+
+
+class Employee(db.Model):
+    __tablename__ = 'employees'
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    designation = db.Column(db.String(255), nullable=False)
+    doj = db.Column(db.Date(), nullable=False)
+    dl = db.relationship('DriverLicense', backref='employee', uselist=False)
+
+
+class DriverLicense(db.Model):
+    __tablename__ = 'driverlicense'
+    id = db.Column(db.Integer(), primary_key=True)
+    license_number = db.Column(db.String(255), nullable=False)
+    renewed_on = db.Column(db.Date(), nullable=False)
+    expiry_date = db.Column(db.Date(), nullable=False)
+    employee_id = db.Column(db.Integer(), db.ForeignKey('employees.id'))
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', name='Jerry')
+
+
+@app.route('/user/<int:user_id>/')
+def user_profile(user_id):
+    return "Profile page of user #{}".format(user_id)
+
+
+@app.route('/books/<genre>/')
+def books(genre):
+    res = make_response("All Books in {} category".format(genre))
+    res.headers['Content-Type'] = 'text/plain'
+    res.headers['Server'] = 'Foobar'
+    return res
+
+
+@app.route('/login/', methods=['post', 'get'])
+def login():
+    message = ''
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+    if username == 'root' and password == 'pass':
+        message = "Correct username and password"
+    else:
+        message = "Wrong username or password"
+
+    return render_template('login.html', message=message)
+
+
+@app.route('/contact/', methods=['get', 'post'])
+def contact():
+    form = ContactForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        email = form.email.data
+        message = form.message.data
+        print(name)
+        print(email)
+        print(message)
+        print("\nData received. Now redirecting ...")
+        flash("Message Received", "success")
+        return redirect(url_for('contact'))
+
+    return render_template('contact.html', form=form)
+
+
+@app.route('/cookie/')
+def cookie():
+    if not request.cookies.get('foo'):
+        res = make_response("Setting a cookie")
+        res.set_cookie('foo', 'bar', max_age=60 * 60 * 24 * 365 * 2)
+    else:
+        res = make_response("Value of cookie foo is {}".format(request.cookies.get('foo')))
+    return res
+
+
+@app.route('/delete-cookie/')
+def delete_cookie():
+    res = make_response("Cookie Removed")
+    res.set_cookie('foo', 'bar', max_age=0)
+    return res
+
+
+@app.route('/article/', methods=['POST', 'GET'])
+def article():
+    if request.method == 'POST':
+        print(request.form)
+        res = make_response("")
+        res.set_cookie("font", request.form.get('font'), 60 * 60 * 24 * 15)
+        res.headers['location'] = url_for('article')
+        return res, 302
+
+    return render_template('article.html')
+
+
+@app.route('/')
+def http_404_handler():
+    return make_response("<h2>404 Error</h2>", 400)
+
+
+@app.route('/set-cookie')
+def set_cookie():
+    res = make_response("Cookie setter")
+    res.set_cookie("favorite-color", "skyblue", 60 * 60 * 24 * 15)
+    res.set_cookie("favorite-font", "sans-serif", 60 * 60 * 24 * 15)
+    return res
+
+
+@app.route('/')
+def http_500_handler():
+    return "<h2>500 Error</h2>", 500
+
+
+@app.route('/')
+def render_markdown():
+    return "## Heading", 200, {'Content-Type': 'text/markdown'}
+
+
+app.debug = True
+app.config['SECRET_KEY'] = 'a really really really really long secret key'
+
+manager = Manager(app)
+
+if __name__ == "__main__":
+    app.run()
